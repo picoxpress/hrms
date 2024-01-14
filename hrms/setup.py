985a7618ec9253c02f19a375c1c0f37ed24cb970
mrRobot126 <hardik.github1347@gmail.com>
2024-01-14 18:04:39 +0530

    only ignore employee custom field

--- hrms/setup.py
+++ hrms/setup.py
@@ -12,7 +12,7 @@ from hrms.overrides.company import delete_company_fixtures
 
 
 def after_install():
-	# create_custom_fields(get_custom_fields())
+	create_custom_fields(get_custom_fields())
 	make_fixtures()
 	setup_notifications()
 	update_hr_defaults()
@@ -24,113 +24,113 @@ def after_install():
 
 
 def before_uninstall():
-	# delete_custom_fields(get_custom_fields())
+	delete_custom_fields(get_custom_fields())
 	delete_company_fixtures()
 
 
 def get_custom_fields():
 	"""HR specific custom fields that need to be added to the masters in ERPNext"""
 	return {
-		"Employee": [
-			{
-				"fieldname": "employment_type",
-				"fieldtype": "Link",
-				"ignore_user_permissions": 1,
-				"label": "Employment Type",
-				"oldfieldname": "employment_type",
-				"oldfieldtype": "Link",
-				"options": "Employment Type",
-				"insert_after": "department",
-			},
-			{
-				"fieldname": "job_applicant",
-				"fieldtype": "Link",
-				"label": "Job Applicant",
-				"options": "Job Applicant",
-				"insert_after": "employment_details",
-			},
-			{
-				"fieldname": "grade",
-				"fieldtype": "Link",
-				"label": "Grade",
-				"options": "Employee Grade",
-				"insert_after": "branch",
-			},
-			{
-				"fieldname": "default_shift",
-				"fieldtype": "Link",
-				"label": "Default Shift",
-				"options": "Shift Type",
-				"insert_after": "holiday_list",
-			},
-			{
-				"collapsible": 1,
-				"fieldname": "health_insurance_section",
-				"fieldtype": "Section Break",
-				"label": "Health Insurance",
-				"insert_after": "health_details",
-			},
-			{
-				"fieldname": "health_insurance_provider",
-				"fieldtype": "Link",
-				"label": "Health Insurance Provider",
-				"options": "Employee Health Insurance",
-				"insert_after": "health_insurance_section",
-			},
-			{
-				"depends_on": "eval:doc.health_insurance_provider",
-				"fieldname": "health_insurance_no",
-				"fieldtype": "Data",
-				"label": "Health Insurance No",
-				"insert_after": "health_insurance_provider",
-			},
-			{
-				"fieldname": "approvers_section",
-				"fieldtype": "Section Break",
-				"label": "Approvers",
-				"insert_after": "default_shift",
-			},
-			{
-				"fieldname": "expense_approver",
-				"fieldtype": "Link",
-				"label": "Expense Approver",
-				"options": "User",
-				"insert_after": "approvers_section",
-			},
-			{
-				"fieldname": "leave_approver",
-				"fieldtype": "Link",
-				"label": "Leave Approver",
-				"options": "User",
-				"insert_after": "expense_approver",
-			},
-			{
-				"fieldname": "column_break_45",
-				"fieldtype": "Column Break",
-				"insert_after": "leave_approver",
-			},
-			{
-				"fieldname": "shift_request_approver",
-				"fieldtype": "Link",
-				"label": "Shift Request Approver",
-				"options": "User",
-				"insert_after": "column_break_45",
-			},
-			{
-				"fieldname": "salary_cb",
-				"fieldtype": "Column Break",
-				"insert_after": "salary_mode",
-			},
-			{
-				"fetch_from": "department.payroll_cost_center",
-				"fetch_if_empty": 1,
-				"fieldname": "payroll_cost_center",
-				"fieldtype": "Link",
-				"label": "Payroll Cost Center",
-				"options": "Cost Center",
-				"insert_after": "salary_cb",
-			},
-		],
+		# "Employee": [
+		# 	{
+		# 		"fieldname": "employment_type",
+		# 		"fieldtype": "Link",
+		# 		"ignore_user_permissions": 1,
+		# 		"label": "Employment Type",
+		# 		"oldfieldname": "employment_type",
+		# 		"oldfieldtype": "Link",
+		# 		"options": "Employment Type",
+		# 		"insert_after": "department",
+		# 	},
+		# 	{
+		# 		"fieldname": "job_applicant",
+		# 		"fieldtype": "Link",
+		# 		"label": "Job Applicant",
+		# 		"options": "Job Applicant",
+		# 		"insert_after": "employment_details",
+		# 	},
+		# 	{
+		# 		"fieldname": "grade",
+		# 		"fieldtype": "Link",
+		# 		"label": "Grade",
+		# 		"options": "Employee Grade",
+		# 		"insert_after": "branch",
+		# 	},
+		# 	{
+		# 		"fieldname": "default_shift",
+		# 		"fieldtype": "Link",
+		# 		"label": "Default Shift",
+		# 		"options": "Shift Type",
+		# 		"insert_after": "holiday_list",
+		# 	},
+		# 	{
+		# 		"collapsible": 1,
+		# 		"fieldname": "health_insurance_section",
+		# 		"fieldtype": "Section Break",
+		# 		"label": "Health Insurance",
+		# 		"insert_after": "health_details",
+		# 	},
+		# 	{
+		# 		"fieldname": "health_insurance_provider",
+		# 		"fieldtype": "Link",
+		# 		"label": "Health Insurance Provider",
+		# 		"options": "Employee Health Insurance",
+		# 		"insert_after": "health_insurance_section",
+		# 	},
+		# 	{
+		# 		"depends_on": "eval:doc.health_insurance_provider",
+		# 		"fieldname": "health_insurance_no",
+		# 		"fieldtype": "Data",
+		# 		"label": "Health Insurance No",
+		# 		"insert_after": "health_insurance_provider",
+		# 	},
+		# 	{
+		# 		"fieldname": "approvers_section",
+		# 		"fieldtype": "Section Break",
+		# 		"label": "Approvers",
+		# 		"insert_after": "default_shift",
+		# 	},
+		# 	{
+		# 		"fieldname": "expense_approver",
+		# 		"fieldtype": "Link",
+		# 		"label": "Expense Approver",
+		# 		"options": "User",
+		# 		"insert_after": "approvers_section",
+		# 	},
+		# 	{
+		# 		"fieldname": "leave_approver",
+		# 		"fieldtype": "Link",
+		# 		"label": "Leave Approver",
+		# 		"options": "User",
+		# 		"insert_after": "expense_approver",
+		# 	},
+		# 	{
+		# 		"fieldname": "column_break_45",
+		# 		"fieldtype": "Column Break",
+		# 		"insert_after": "leave_approver",
+		# 	},
+		# 	{
+		# 		"fieldname": "shift_request_approver",
+		# 		"fieldtype": "Link",
+		# 		"label": "Shift Request Approver",
+		# 		"options": "User",
+		# 		"insert_after": "column_break_45",
+		# 	},
+		# 	{
+		# 		"fieldname": "salary_cb",
+		# 		"fieldtype": "Column Break",
+		# 		"insert_after": "salary_mode",
+		# 	},
+		# 	{
+		# 		"fetch_from": "department.payroll_cost_center",
+		# 		"fetch_if_empty": 1,
+		# 		"fieldname": "payroll_cost_center",
+		# 		"fieldtype": "Link",
+		# 		"label": "Payroll Cost Center",
+		# 		"options": "Cost Center",
+		# 		"insert_after": "salary_cb",
+		# 	},
+		# ],
 		"Company": [
 			{
 				"fieldname": "hr_settings_section",
